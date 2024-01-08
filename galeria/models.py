4805210f8d515665a4e32af9f89910cd3011905d
cabraalluana <luanaoliveira1309@gmail.com>
2024-01-08 20:57:28 -0300

    Mecanismo de busca

--- galeria/models.py
+++ galeria/models.py
@@ -5,9 +5,9 @@ class Fotografia(models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
     descricao = models.TextField(null=False, blank=False)
-    foto = models.CharField(max_length=150, null=False, blank=False)
+    foto = models.ImageField(upload_to="foto/%Y/%m/%d/", blank=True)
     publicada = models.BooleanField(default=False)
     data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
 
 def __str__(self):
-    return f"Fotografia [nome={self.nome}]"
+    return self.nome
