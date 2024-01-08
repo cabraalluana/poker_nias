d83a9b880d36c87234674c3e4bd4e1e246178e7e
cabraalluana <luanaoliveira1309@gmail.com>
2024-01-08 19:04:51 -0300

    Avanço no admin

--- galeria/models.py
+++ galeria/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from datetime import datetime
 
 class Fotografia(models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=150, null=False, blank=False)
+    publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
 
 def __str__(self):
     return f"Fotografia [nome={self.nome}]"
