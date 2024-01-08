da6044d13fd4d0fdfd30fb15ff9033841b38cc75
cabraalluana <luanaoliveira1309@gmail.com>
2024-01-08 15:51:23 -0300

    Lidando com dados

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+        1:{"nome": "Regras", "legenda": "Aprenda as regras do Poker."},
+        2:{"nome": "Como jogar?", "legenda": "Aprenda a submeter seu código."},
+        3:{"nome": "Jogar", "legenda": "Acesse sua conta ou faça um cadastro."},
+        4:{"nome": "Ranking", "legenda": "Ranking de Poker - Ganhos e Perdas."}
+    }
+
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
